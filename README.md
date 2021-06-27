77e4ce572dfc60333dd2c8c53db435c1726d1749
EC2 Default User <ec2-user@ip-172-31-26-240.us-east-2.compute.internal>
2021-06-27 13:48:08 +0000

    updated demo for cdk8s version 1.0.0-beta.26

--- README.md
+++ README.md
@@ -1,28 +1,19 @@
 You can find the PPT of my session at https://www.slideshare.net/rohinigaonkar/infrastructure-as-code-with-aws-cloud-development-kit-cdk
 
-# kubHello
-CDK8s Hello Sample app. 
+CDK8s Hello Sample app.
 
 ======================================================================
 
- Your cdk8s typescript project is ready!
+Your cdk8s typescript project is ready!
 
-   cat help         Print this message
- 
-  Compile:
-   npm run compile     Compile typescript code to javascript (or "yarn watch")
-   npm run watch       Watch for changes and compile typescript in the background
-   npm run build       Compile + synth
+cat help Print this message
 
-  Synthesize:
-   npm run synth       Synthesize k8s manifests from charts to dist/ (ready for 'kubectl apply -f')
+Compile: npm run compile Compile typescript code to javascript (or "yarn watch") npm run watch Watch for changes and compile typescript in the background npm run build Compile + synth
 
- Deploy:
-   kubectl apply -f dist/*.k8s.yaml
+Synthesize: npm run synth Synthesize k8s manifests from charts to dist/ (ready for 'kubectl apply -f')
 
- Upgrades:
-   npm run import        Import/update k8s apis (you should check-in this directory)
-   npm run upgrade       Upgrade cdk8s modules to latest version
-   npm run upgrade:next  Upgrade cdk8s modules to latest "@next" version (last commit)
+Deploy: kubectl apply -f dist/*.k8s.yaml
+
+Upgrades: npm run import Import/update k8s apis (you should check-in this directory) npm run upgrade Upgrade cdk8s modules to latest version npm run upgrade:next Upgrade cdk8s modules to latest "@next" version (last commit)
 
 ================================================================
--- main.ts
+++ main.ts
@@ -1,53 +1,62 @@
-import { Construct } from 'constructs';
-import { App, Chart } from 'cdk8s';
+import { Construct } from "constructs";
+import { App, Chart, ChartProps } from "cdk8s";
 
-import { Service, IntOrString, Deployment } from './imports/k8s';
+import { KubeService, IntOrString, KubeDeployment } from "./imports/k8s";
 
 export class MyChart extends Chart {
-  constructor(scope: Construct, name: string) {
-    super(scope, name);
-    // define resources here
-    const label = { app: 'hello-k8s' };
+  constructor(scope: Construct, id: string, props: ChartProps = {}) {
+    super(scope, id, props);
 
-    new Service(this, 'service', {
+    // define resources here
+
+    //Create Label
+    const label = { app: "hello-k8s" };
+
+    //Create Service
+    new KubeService(this, "service", {
       spec: {
-        type: 'LoadBalancer',
-        ports: [{ 
-          port: 80, 
-          targetPort: IntOrString.fromNumber(8080) }],
-        selector: label
-      }
+        type: "LoadBalancer",
+        ports: [
+          {
+            port: 80,
+            targetPort: IntOrString.fromNumber(8080),
+          },
+        ],
+        selector: label,
+      },
     });
 
-    new Deployment(this, 'deployment', {
+    //Create Deployment
+    new KubeDeployment(this, "deployment", {
       spec: {
         replicas: 1,
         selector: {
-          matchLabels: label
+          matchLabels: label,
         },
         template: {
           metadata: { labels: label },
           spec: {
             containers: [
               {
-                name: 'hello-kubernetes',
-                image: 'paulbouwer/hello-kubernetes:1.8',
+                name: "hello-kubernetes",
+                image: "paulbouwer/hello-kubernetes:1.8",
                 ports: [{ containerPort: 8080 }],
-                env: [{
-                  name: "MESSAGE",
-                  value: "I just deployed this on AWS EKS using CDK for Kubernetes"
-
-                }]
-              }
-            ]
-          }
-        }
-      }
+                env: [
+                  {
+                    name: "MESSAGE",
+                    value:
+                      "I just deployed this on AWS EKS using CDK for Kubernetes",
+                  },
+                ],
+              },
+            ],
+          },
+        },
+      },
     });
-
   }
 }
 
 const app = new App();
-new MyChart(app, 'kubHello');
+new MyChart(app, "aws-cdk8s-demo");
 app.synth();
